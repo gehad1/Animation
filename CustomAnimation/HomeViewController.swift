96ed19ba2388ac3ec16ceb1e2eae414197da514a
gehadgamal046 <gehadgamal046@gmail.com>
2020-02-13 18:57:03 +0200

    start to animate the three views in home screen

--- CustomAnimation/HomeViewController.swift
+++ CustomAnimation/HomeViewController.swift
@@ -13,15 +13,36 @@ class HomeViewController: UIViewController {
 
  
     @IBOutlet weak var tutorView: AnimatableView!
-    
+    @IBOutlet weak var whoLabel: UILabel!
     @IBOutlet weak var babySitterView: AnimatableView!
-    
     @IBOutlet weak var discoveriesView: AnimatableView!
+    
+    let info = UILabel()
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        animateFrom()
+    }
+    func animateFrom() {
+        // TODO: add your frist layer animation
+        let flyRight = CABasicAnimation(keyPath: "position.x")
+        flyRight.duration = 1
+        flyRight.fromValue = -view.bounds.size.width / 2
+        flyRight.toValue = view.bounds.size.width / 4 + 4
+        tutorView.layer.add(flyRight, forKey: nil)
+        babySitterView.layer.add(flyRight, forKey: nil)
+        discoveriesView.layer.add(flyRight, forKey: nil)
+    }
+    
+//    func animateInfo() {
+//        info.frame = CGRect(x: 0.0, y: whoLabel.center.y + 30, width: view.frame.size.width, height: 30)
+//        view.insertSubview(info, belowSubview: whoLabel)
+//    }
 
     @IBAction func tutorButtonTapped(_ sender: Any) {
     }
